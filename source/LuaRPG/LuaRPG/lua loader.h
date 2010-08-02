fd74be493fc77cdca9aa989597f3a08ea4c2694a
swdgame.tien <swdgame.tien@1c9e41e7-7468-c316-5b86-e9b6d175c192>
2010-08-02 08:39:08 +0000

    lua loader 0.2 version added: get field method , get array method, get double method
    main.cpp verified for testing lua loader

--- source/LuaRPG/LuaRPG/lua loader.h
+++ source/LuaRPG/LuaRPG/lua loader.h
@@ -33,15 +33,31 @@ public:
 
 	//
 	virtual	int	GetInt( int code, const std::string& val_name );
-	//virtual	double	GetDouble( int code, const std::string& val_name );
+	virtual	double	GetDouble( int code, const std::string& val_name );
 	virtual	std::string	GetString( int code, const std::string& val_name );
+
+	virtual	bool	GetInt( int code, const std::string& val_name, int& target );
+	virtual	bool	GetDouble( int code, const std::string& val_name, double& target );
+	virtual	bool	GetString( int code, const std::string& val_name, std::string& target );
 
 	//
 	//virtual	int	GetIntFromFile( int* code,	const std::string& file_name, const std::string& val_name );
 	//virtual	double	GetDoubleFromFile( int* code, std::string& file_name, const std::string& val_name );
 	//virtual	std::string	GetStringFromFile( int* code, std::string& file_name, const std::string& val_name );
 
-
+	virtual	bool	GetFieldBegin( int code, const std::string& table_name );
+	virtual	bool	GetFieldEnd( int code );
+	virtual	bool	GetFieldInt( int code, const std::string& val_name, int& target );
+	virtual	bool	GetFieldInt( int code, const int& val_name, int& target );
+	virtual	bool	GetFieldDouble( int code, const std::string& val_name, double& target );
+	virtual	bool	GetFieldDouble( int code, const int& val_name, double& target );
+	virtual	bool	GetFieldString( int code, const std::string& val_name, std::string& target );
+	virtual	bool	GetFieldString( int code, const int& val_name, std::string& target );
+	
+	
+	virtual	bool	GetArrayInt( int code, const std::string& table_name, int* arr, int index_start, int index_end );
+	virtual	bool	GetArrayDouble( int code, const std::string& table_name, double* arr, int index_start, int index_end );
+	virtual	bool	GetArrayString( int code, const std::string& table_name, std::string* arr, int index_start, int index_end );
 protected:
 	virtual	int	ValidCode( int code );
 
